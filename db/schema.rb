b633901251fdb4b803b3c4ea9f3fd865871afc0f
Devansh Batra <devanshbatra04@gmail.com>
2018-06-26 02:43:23 +0530

    db update

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_25_210012) do
+ActiveRecord::Schema.define(version: 2018_06_25_211000) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
+    t.text "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
