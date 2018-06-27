cef7b72f394a4b89de90cab1d46596bb1371ad59
Devansh Batra <devanshbatra04@gmail.com>
2018-06-27 22:26:34 +0530

    form validation now handled in create route

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,8 +5,14 @@ class ArticlesController < ApplicationController
   def create
     # render plain: params[:article].inspect
     @article = Article.new(article_params)
-    @article.save
-    redirect_to articles_path(@article)
+    # @article.save
+    # redirect_to articles_path(@article)
+    if @article.save
+      flash[:notice] = "Article was successfully created"
+      redirect_to articles_path(@article)
+    else
+      render 'new'
+    end
   end
   private
   def article_params
