1dbc602b6cb1b883f1cfe69ae5ffad83b330b04b
Devansh Batra <devanshbatra04@gmail.com>
2018-06-27 23:14:38 +0530

    Show page working finally

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,16 +9,18 @@ class ArticlesController < ApplicationController
     # redirect_to articles_path(@article)
     if @article.save
       flash[:notice] = "Article was successfully created"
-      redirect_to articles_path(@article)
+      redirect_to article_path(@article)
     else
       render 'new'
     end
   end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
   private
   def article_params
     params.require(:article).permit(:title, :description)
-  end
-  def show
-
   end
 end
