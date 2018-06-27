c3f621d73cbac9a0265cfa145b5400eb854f2fcd
Devansh Batra <devanshbatra04@gmail.com>
2018-06-27 22:08:02 +0530

    params added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,6 +6,7 @@ class ArticlesController < ApplicationController
     # render plain: params[:article].inspect
     @article = Article.new(article_params)
     @article.save
+    redirect_to articles_show(@article)
   end
   private
   def article_params
